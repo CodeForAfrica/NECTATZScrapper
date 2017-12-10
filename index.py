b290a6d85c69c4cdeaff6eba923443a52fa2dceb
khadija <khadijam@saratoga.co.za>
2017-12-10 11:39:07 +0300

    script for gender based info

--- index.py
+++ index.py
@@ -1,40 +1,63 @@
-#=IMPORTXML("http://www.necta.go.tz/results/2014/acsee/index.htm", "//a/@href")
-
-
+import sys
+import json
+import os
+from os.path import join
+import config as config
 from lxml import html
 import requests
 
-page = requests.get('http://www.necta.go.tz/results/2014/acsee/index.htm', allow_redirects = False)
+try:
 
-#tree contains the whole html
-tree = html.fromstring(page.content)
-htmlElem = html.document_fromstring(page.content)
+	page = requests.get(config.cseeYear2016['resultUrl'], allow_redirects = False)
 
-
-#list of schools and uri
-
-schools = tree.xpath('//a/text()')
-schoolsUri = tree.xpath('//a/@href')
-resultTable = tree.xpath('//tr/td')
-resultTable2 = tree.xpath('//td')[0].text_content().strip()
-data = list()
-#for row in resultTable:
- #   data.append([c.text for c in row.getchildren()])
+	#tree contains the whole html
+	tree = html.fromstring(page.content)
+	htmlElem = html.document_fromstring(page.content)
 
 
-#print schoolsUri
-#print schools
-print schools
-print len(schoolsUri)
+	schools = tree.xpath('//table[@cellspacing="2"]//a/text()')
+	schoolsUri = tree.xpath('//table[@cellspacing="2"]//a/@href')
+	total = len(schools)
+	
+	output_dir='output'
+	os.makedir(output_dir)
 
-#loop through the school names to createfile
-for school in schools:
-	schoolCode = school.split(' ')[0]
-	#print schoolCode
-	csvFileName = schoolCode + ".csv"
-	csv = open(csvFileName, "w") #create or open csv, "w" to write strings
-	colNames = "CNO, SEX, AGGT, DIV, DETAILED SUBJECTS\n" #column titles
-	#csv.write(colNames)
+
+	for i in range (0, total):
+	
+		school = schools[i].strip()
+		if not (school.startswith('P') or school.startswith('p')):
+			school = school.split()
+			school_code = school[0]
+			del school[0]
+			school_name = " ".join(school)
+
+			csvFileName = school_code + ".csv"
+			csv = open(os.path.join(output_dir, csvFileName), "w") #create or open csv, "w" to write strings
+			colNames = "GENDER, DIVISION\n" #column titles
+			csv.write(colNames)
+			print total
+			school_uri = schoolsUri[i].replace("\\","/")
+
+			#Get school specific html content
+			schoolResult = requests.get(config.cseeYear2016['schoolPath'] + school_uri, allow_redirects = False)
+			schoolTree = html.fromstring(schoolResult.content)
+		
+			resultTdElem = schoolTree.xpath('//table[@cellspacing="2"]//td')
+			resultTdElemLength = len(resultTdElem)
+
+			if resultTdElemLength > 0:
+				for x in range (5, resultTdElemLength-5, 5):
+					gender = resultTdElem[x+1]
+					division = resultTdElem[x+3]
+					csv.write(gender +", "+ division + "\n")
+				
+
+except Exception:
+	print "Oops, something went wrong"
+
+
+sys.exit()
 
 
 
